e7129516b0f180134862f1582a84d898d612b5eb
rshin808 <rshin808@gmail.com>
2015-04-29 12:56:41 -1000

    Fixing

--- TestSite/CSBCD/signups/forms.py
+++ TestSite/CSBCD/signups/forms.py
@@ -4,4 +4,4 @@ from .models import SignUp
 class SignUpForm(forms.ModelForm):
     class Meta:
         model = SignUp
-	fields = __all__;
+	field = __all__
--- TestSite/CSBCD/temperatures/views.py
+++ TestSite/CSBCD/temperatures/views.py
@@ -1,14 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Temperature
-import json
+from django.http import JsonResponse
 
 def chart_data_json(request):
-    to_json = []
-    for i in range(10):
-        data_dict = {}
-        data_dict['timestamp'] = i
-        data_dict['values'] = i
-        to_json.append(data_dict)        
-    response_data = json.dumps(to_json)
-    return HttpResponse(response_data, content_type="application/json")
+    return JsonResponse({"foo": "bar"})    
